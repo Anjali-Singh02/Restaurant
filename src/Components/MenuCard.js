efac243ff7b1ea194a831648c54b3b03b613e091
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-06-02 16:38:33 +0530

    destructured elements

--- src/Components/MenuCard.js
+++ src/Components/MenuCard.js
@@ -1,37 +1,46 @@
-import React, { Fragment }  from 'react'
+import React, { Fragment } from 'react';
 
-const MyCard = ({menuData}) => {
-  // console.log(menuData);
-  return (
+const MyCard = ({ menuData }) => {
+	// console.log(menuData);
+	return (
+		<Fragment>
+			<section className="main-card--cointainer">
+				{menuData.map((curElement) => {
+					const { id, category, name, description, image } =
+						curElement;
+					return (
+						<>
+							<div className="card-container" key={id}>
+								<div className=" card ">
+									<div className="card-body">
+										<span className="card-number card-circle subtle">
+											{id}
+										</span>
+										<span className="card-author subtle">
+											{category}
+										</span>
+										<h2 className="card-title">{name}</h2>
+										<span className="card-description subtle">
+											{description}
+										</span>
+										<div className="card-read">Read</div>
+										<img
+											src={image}
+											alt="images"
+											className="card-media"
+										/>
+										<span className="card-tag subtle">
+											Order now
+										</span>
+									</div>
+								</div>
+							</div>
+						</>
+					);
+				})}
+			</section>
+		</Fragment>
+	);
+};
 
-
-    <Fragment>
-      <section className='main-card--cointainer'>
-
-   
-      {menuData.map((curElement => {
-        return (
-          <>
-          <div className='card-container' key={curElement.id}>
-        <div className=' card '>
-            <div className='card-body'>
-                <span className='card-number card-circle subtle'>{curElement.id}</span>
-                <span className='card-author subtle'>{curElement.category}</span>
-                <h2 className='card-title'>{curElement.name}</h2>
-                <span className='card-description subtle'>{curElement.description}
-                </span>
-                <div className='card-read'>Read</div>
-                <img src={curElement.image} alt="images" className='card-media' />
-                <span className='card-tag subtle'>Order now</span>
-            </div>
-        </div>
-    </div>
-          </>
-        )
-      }))}
-      </section>
-  </Fragment>
-  )
-}
-
-export default MyCard
+export default MyCard;
